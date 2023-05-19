fb984f207f6e2acfdac22b45cb6d91ba47d63124
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-05-19 19:36:29 +0300

    Update Deck.cpp

--- Blackjack/Blackjack/Deck.cpp
+++ Blackjack/Blackjack/Deck.cpp
@@ -1,1 +1,38 @@
 #include "Deck.h"
+#include <iostream>
+
+
+void createDeck(Deck* const& deck)
+{
+	// We could initialize each card individually, but that would be a pain.  Let's use a loop.
+	using Index = size_t;
+
+	Index index{ 0 };
+
+	for (int suit{ 0 }; suit < static_cast<int>(Card::CardSuit::max_suits); ++suit)
+	{
+		for (int rank{ 0 }; rank < static_cast<int>(Card::CardRank::max_ranks); ++rank)
+		{
+			deck->m_deck[index] = Card(
+				static_cast<Card::CardRank>(rank),
+				static_cast<Card::CardSuit>(suit));
+			++index;
+		}
+	}
+}
+
+Deck::Deck()
+{
+	createDeck(this);
+}
+
+void Deck::print() const
+{
+	for (const auto& card : m_deck)
+	{
+		card.print();
+		std::cout << ' ';
+	}
+
+	std::cout << '\n';
+}
