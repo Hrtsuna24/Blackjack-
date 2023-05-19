cc695fdad2b9009aa17061701625eacb2676c81c
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-05-19 19:59:56 +0300

    Update Player.cpp

---
+++ Blackjack/Blackjack/Player.cpp
@@ -0,0 +1,61 @@
+#include "Player.h"
+#include "Deck.h"
+#include "Card.h"
+
+int Player::drawCard(const Deck& deck) const
+{
+	int val = deck.dealCard().value();
+
+	return val;
+}
+
+
+//bool playerTurn(const Deck& deck, Index& nextCardIndex, Player& player)
+//{
+//    while (true)
+//    {
+//        if (player.score > g_maximumScore)
+//        {
+//            // This can happen even before the player had a choice if they drew 2
+//            // aces.
+//            std::cout << "You busted!\n";
+//            return true;
+//        }
+//        else
+//        {
+//            if (playerWantsHit())
+//            {
+//                int cardValue{ getCardValue(deck.at(nextCardIndex++)) };
+//                player.score += cardValue;
+//                std::cout << "You were dealt a " << cardValue << " and now have " << player.score << '\n';
+//            }
+//            else
+//            {
+//                // The player didn't go bust.
+//                return false;
+//            }
+//        }
+//    }
+//}
+//
+//// Returns true if the dealer went bust. False otherwise.
+//bool dealerTurn(const Deck& deck, Index& nextCardIndex, Player& dealer)
+//{
+//    // Draw cards until we reach the minimum value.
+//    while (dealer.score < g_minimumDealerScore)
+//    {
+//        int cardValue{ getCardValue(deck.at(nextCardIndex++)) };
+//        dealer.score += cardValue;
+//        std::cout << "The dealer turned up a " << cardValue << " and now has " << dealer.score << '\n';
+//
+//    }
+//
+//    // If the dealer's score is too high, they went bust.
+//    if (dealer.score > g_maximumScore)
+//    {
+//        std::cout << "The dealer busted!\n";
+//        return true;
+//    }
+//
+//    return false;
+//}
