6b25565abd001df6790650a421e958e385382c3e
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-05-19 19:35:32 +0300

    Update Deck.h

--- Blackjack/Blackjack/Deck.h
+++ Blackjack/Blackjack/Deck.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <array>
+using std::array;
+#include "Card.h"
+
 class Deck
 {
+private:
+	static constexpr size_t Cards{ 52 };
+	using size_type = array<Card, Cards>;
+	size_type m_deck;
+	friend void createDeck(Deck* const& dec);
+
+public:
+
+
+	Deck();
+	void print() const;
 };
 
